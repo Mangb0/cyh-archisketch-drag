718bf5f7faac08c41572e37a8f84479dc55b4047
Mangb0 <whdudghks45629@gmail.com>
2024-03-14 00:00:49 +0900

    Orthographic Scene 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
+import { OrbitControls, Plane } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 5] }}>
+      <Plane args={[5, 5]} />
+      <OrbitControls />
+    </Canvas>
   );
 }
 

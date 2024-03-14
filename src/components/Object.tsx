6b78f1f0e7100b97642b03c6015f51b87fb8f8a2
Mangb0 <whdudghks45629@gmail.com>
2024-03-15 03:32:56 +0900

    Mesh 드래그 이동 효과 추가

--- src/components/Object.tsx
+++ src/components/Object.tsx
@@ -1,5 +1,5 @@
 import { useThree } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useGesture } from "react-use-gesture";
 import * as THREE from "three";
 
@@ -13,6 +13,9 @@ interface Props {
 function Object({ sides, id, startPosition, color }: Props) {
   // Mesh ref 생성
   const meshRef = useRef<THREE.Mesh>(null);
+
+  // 드래그 이동 Mesh 위치
+  const [position, setPosition] = useState<THREE.Vector3 | null>(null);
 
   // canvas, viewport 크기 정보
   const { size, viewport } = useThree();
@@ -30,26 +33,46 @@ function Object({ sides, id, startPosition, color }: Props) {
 
   // 드래그 이벤트
   const bind = useGesture({
+    onDragStart: () => {
+      if (meshRef.current) {
+        setPosition(meshRef.current.position);
+      }
+    },
     onDrag: ({ offset: [x, y] }) => {
-      meshRef.current?.position.set(
-        startPosition[0] + x / aspect,
-        startPosition[1] - y / aspect,
-        0
-      );
+      const newX = startPosition[0] + x / aspect;
+      const newY = startPosition[1] + -y / aspect;
+      setPosition(new THREE.Vector3(newX, newY, 0));
+    },
+    onDragEnd: () => {
+      if (meshRef.current && position) {
+        meshRef.current.position.copy(position);
+        setPosition(null);
+      }
     },
   });
 
   return (
-    <mesh
-      ref={meshRef}
-      name={id}
-      position={startPosition}
-      rotation={[0, 0, -Math.PI / 2 + Math.PI / sides]}
-      {...bind()}
-    >
-      <shapeGeometry args={[shape]} />
-      <meshBasicMaterial color={color} />
-    </mesh>
+    <>
+      <mesh
+        ref={meshRef}
+        name={id}
+        position={startPosition}
+        rotation={[0, 0, -Math.PI / 2 + Math.PI / sides]}
+        {...bind()}
+      >
+        <shapeGeometry args={[shape]} />
+        <meshBasicMaterial color={color} />
+      </mesh>
+      {position && (
+        <mesh
+          position={position}
+          rotation={[0, 0, -Math.PI / 2 + Math.PI / sides]}
+        >
+          <shapeGeometry args={[shape]} />
+          <meshBasicMaterial wireframe={true} />
+        </mesh>
+      )}
+    </>
   );
 }
 
